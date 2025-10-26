db3b471a15ea301e42f2e272ecd23fd7306e5beb
jsotomolinez <jsotomolinez12@gmail.com>
2025-10-25 21:46:21 -0400

    Fix formatting: adjust className and id attributes for consistency in Stack component

--- components/sections/stack.tsx
+++ components/sections/stack.tsx
@@ -8,8 +8,7 @@ export default function Stack() {
 
   return (
     <section
-      className={`py-12 md:py-24 lg:py-32 transition-opacity duration-1500 ease-initial ${isVisible ? "opacity-100" : "opacity-0"}`}
-      id="stack"
+      className={`py-12 md:py-24 lg:py-32 transition-opacity duration-1500 ease-initial ${isVisible ? "opacity-100" : "opacity-0"}`} id="stack"
     >
       <div className="mx-auto flex max-w-[980px] flex-col items-center gap-4 text-center">
         <h1 className="text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:text-6xl">
